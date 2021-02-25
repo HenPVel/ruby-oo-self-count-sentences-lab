54256e3dcf9a189f6cb85d4325762e1e28fa00f2
Henry <hvelas93@gmail.com>
2021-02-24 19:45:45 -0500

    finished counting sentences lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+    #binding.pry
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    counter=0
+    self.split.each do |x|
+      if x.sentence? || x.question? || x.exclamation?
+        counter+=1 
+      end
+    end
+    counter
   end
+
 end
